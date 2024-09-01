48cb9c9d47427a0a40a2459a022307366bcddf0e
Alexis Breton <alexis95150@gmail.com>
2024-09-01 17:22:53 +0200

    Some packaging tweaks

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,17 +3,18 @@ import resolve from '@rollup/plugin-node-resolve'
 import external from 'rollup-plugin-peer-deps-external'
 import postcss from 'rollup-plugin-postcss'
 import sass from 'rollup-plugin-sass'
+import preserveDirectives from 'rollup-preserve-directives'
 
 export default {
   input: 'src/index.tsx',
   output: [
     {
-      file: 'dist/bundle.esm.js',
+      file: 'esm/index.js',
       format: 'esm', 
       sourcemap: true,
     },
     {
-      file: 'dist/bundle.js',
+      file: 'cjs/index.js',
       format: 'cjs',
       sourcemap: true,
       name: 'react-lib',
@@ -32,5 +33,6 @@ export default {
       outputStyle: 'compressed',
       includePaths: ['node_modules'],
     }),
+    preserveDirectives()
   ],
 }
